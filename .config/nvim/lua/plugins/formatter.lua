2603946446024c8238f8e1f91fa838c5d7e23166
luviro <luviro@tutamail.com>
2025-04-08 18:22:49 +0300

    fixed formatting for bash scripts

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -10,7 +10,7 @@ return {
             json = { "prettier" },
             jsonc = { "prettier" },
             css = { "prettier" },
-            bash = { "beautysh" },
+            sh = { "beautysh" },
             xml = { "xmlformatter" },
         },
         formatters = {
