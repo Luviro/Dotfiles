a688c6c871a30dfbd0f301a4f16093f6ff1efc6d
luviro <luviro@tutamail.com>
2025-04-17 18:00:11 +0300

    removed unnecessary code in scripts

--- .config/hypr/scripts/appLauncher.sh
+++ .config/hypr/scripts/appLauncher.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Check if rofi is installed
-if ! command -v "rofi" &> /dev/null; then
-    hyprctl notify 3 3000 0 "fontsize:24  Rofi isn't installed"
-    exit 1
-fi
-
 # Check if rofi is already running
 if pgrep -x "rofi" &> /dev/null; then
     hyprctl notify 1 2000 0 "fontsize:24  Rofi is already running"
-	exit 1
+    exit 1
 fi
 
 # run rofi
--- .config/hypr/scripts/hyprPicker.sh
+++ .config/hypr/scripts/hyprPicker.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Check if hyprpicker is installed
-if ! command -v "hyprpicker" &> /dev/null; then
-    hyprctl notify 3 3000 0 "fontsize:24  Hyprpicker isn't installed"
-    exit 1
-fi
-
 # Check if hyprpicker is already running
 if pgrep -x "hyprpicker" &> /dev/null; then
     hyprctl notify 1 2000 0 "fontsize:24  Hyprpicker is already running"
-	exit 1
+    exit 1
 fi
 
 # Run hyprpicker
@@ -17,8 +11,8 @@ color=$(hyprpicker --autocopy)
 
 # Check if a color was selected
 if [ -n "$color" ]; then
-	noHashtagColor=${color#\#}
-	hyprctl notify -1 2000 "rgb($noHashtagColor)" "fontsize:24  $color"
+    noHashtagColor=${color#\#}
+    hyprctl notify -1 2000 "rgb($noHashtagColor)" "fontsize:24  $color"
 else
-	hyprctl notify 4 2000 "rgb(ffffff)" "fontsize:24  No color selected"
+    hyprctl notify 4 2000 "rgb(ffffff)" "fontsize:24  No color selected"
 fi
