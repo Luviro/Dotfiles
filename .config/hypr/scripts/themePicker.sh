f72dd840337608485de77f8e516897f224ceddba
luviro <luviro@tutamail.com>
2025-05-04 14:00:43 +0300

    changed the cp command in the themePicker script so that it copies everything in the themes directories, added an images for the app launcher prompt for hatsune miku theme, and made the fastfetch of hatsune miku theme use the arch ascii

--- .config/hypr/scripts/themePicker.sh
+++ .config/hypr/scripts/themePicker.sh
@@ -12,12 +12,6 @@ themes="Purple/Lime\nHatsune Miku"
 # paths for relevant configs
 config="$HOME/.config"
 
-cava="cava/config"
-clipse="clipse/custom_theme.json"
-fastfetch="fastfetch/config.jsonc"
-rofi="rofi/colors.rasi"
-
-
 themes_dir="$HOME/.config/mythemes"
 
 # run rofi
@@ -26,19 +20,19 @@ picked=$(echo -e "$themes" | rofi -dmenu -theme $HOME/.config/rofi/themePicker.r
 # function for purple-lime theme
 purple_lime(){
     local purple_lime="$themes_dir/purple-lime"
-    cp "$purple_lime/$rofi" "$config/$rofi"
-    cp "$purple_lime/$cava" "$config/$cava"
-    cp "$purple_lime/$clipse" "$config/$clipse"
-    cp "$purple_lime/$fastfetch" "$config/$fastfetch"
+    cp -r "$purple_lime/rofi/." "$config/rofi"
+    cp -r "$purple_lime/cava/." "$config/cava"
+    cp -r "$purple_lime/clipse/." "$config/clipse"
+    cp -r "$purple_lime/fastfetch/." "$config/fastfetch"
 }
 
 # function for hatsune-miku theme
 hatsune_miku(){
     local hatsune_miku="$themes_dir/hatsune-miku"
-    cp "$hatsune_miku/$rofi" "$config/$rofi"
-    cp "$hatsune_miku/$cava" "$config/$cava"
-    cp "$hatsune_miku/$clipse" "$config/$clipse"
-    cp "$hatsune_miku/$fastfetch" "$config/$fastfetch"
+    cp -r "$hatsune_miku/rofi/." "$config/rofi"
+    cp -r "$hatsune_miku/cava/." "$config/cava"
+    cp -r "$hatsune_miku/clipse/." "$config/clipse"
+    cp -r "$hatsune_miku/fastfetch/." "$config/fastfetch"
 }
 
 case $picked in
